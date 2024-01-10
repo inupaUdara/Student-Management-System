46998d94c19b46f4e23531ee3da8aa10bb0fad64
Inupa_Udara <127815279+inupaUdara@users.noreply.github.com>
2024-01-10 12:34:30 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,15 @@
 
 import './App.css';
 import AddStudent from './components/AddStudent';
-
+import AllStudent from './components/AllStudent';
 import Header from './components/Header';
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom"
 import UpdateStudent from './components/UpdateStudent';
 import DeleteStudent from './components/DeleteStudent';
-import AllTeacher from './components/AllTeacher';
-import AllStudent from './components/AllStudent';
-import addTeacher from './components/addTeacher';
-import DeleteTeacher from './components/addTeacher';
-import UpdateTeacher from './components/updateTeacher';
+import UpdateTeacher from './components/updateTeacher.js';
+import AddTeacher from './components/addTeacher.js'
+import deleteTeacher from './components/DeleteTeacher.js'
+import AllTeacher from './components/AllTeacher.js';
 
 function App() {
   return (
@@ -25,10 +24,10 @@ function App() {
           <Route path='/delete/:id' exact Component={DeleteStudent}/>
           <Route path='/' exact Component={AllStudent}/> */}
 
-          <Route path='/' exact Component={addTeacher}/>
+          <Route path='/teachers' exact Component={AddTeacher}/>
           <Route path='/update/:id' exact Component={UpdateTeacher}/>
-          <Route path='/delete/:id' exact Component={DeleteTeacher}/>
-          <Route path='/teachers' exact Component={AllTeacher}/>
+          <Route path='/delete/:id' exact Component={deleteTeacher}/>
+          <Route path='/' exact Component={AllTeacher}/>
         </Routes>
         
          
